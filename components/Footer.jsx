84fbf724fae05fbd4ff5ed689ea63c022e47eb1b
Jaydeep Jangid <105041015+jaydeep-pro@users.noreply.github.com>
2023-05-12 21:29:37 +0530

    Add files via upload

---
+++ components/Footer.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div>
+        <h1>TechyStar</h1>
+        <p>@all rights reserved</p>
+      </div>
+      <div>
+        <h5>Follow Us</h5>
+        <div>
+          <a href="https://youtube.com" target="blank">
+            Youtube
+          </a>
+          <a href="https://instagram.com" target="blank">
+            Instagram
+          </a>
+          <a href="https://github.com" target="blank">
+            Github
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ components/Services.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import img1 from "../assets/3.jpg";
+import img2 from "../assets/4.jpg";
+
+const Services = () => {
+  return (
+    <div>
+      <Carousel
+        infiniteLoop
+        autoPlay
+        showStatus={false}
+        showArrows={false}
+        interval={1000}
+        showThumbs={false}
+        showIndicators={false}
+      >
+        <div>
+          <img src={img1} alt="Item1" />
+          <p className="legend">Full Stack</p>
+        </div>
+        <div>
+          <img src={img2} alt="Item3" />
+          <p className="legend">Peer-to-peer Support</p>
+        </div>
+      </Carousel>
+    </div>
+  );
+};
+
+export default Services;
